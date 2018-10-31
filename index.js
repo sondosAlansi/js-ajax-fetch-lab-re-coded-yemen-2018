12caba06d9a4c3d64baab2911b7f6f6f24591433
sondos alansi <sondos.alansi@yahoo.com>
2018-10-31 19:46:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,11 +12,23 @@ function showResults(json) {
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
+  fetch(repo)
+  .then(resp => resp.json())
+  .then(json => console.log(json));
   //use fetch to fork it!
 }
 
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
+  const token = ' ';
+
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+})
+  .then(res => res.json())
+  .then(json => console.log(json));
   return ''
 }
